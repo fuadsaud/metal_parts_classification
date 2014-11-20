Fix friggin calculation errors

mlp.py
@@ -16,34 +16,37 @@ class MLP:
         self.out_values = [1.0] * self.n_out
 
         self.w_in  = np.random.rand(self.n_in,  self.n_hid)
-        print self.w_in
         self.w_out = np.random.rand(self.n_hid, self.n_out)
-        print self.w_out
 
     def train(self, inputs, target):
         self.run(inputs)
 
-        return self.__update_weights(target)
+        self.__update_weights(target)
 
     def run(self, inputs):
         if len(inputs) != self.n_in: raise ValueError('wrong number of inputs')
 
-        for i, input in enumerate(inputs):
-            self.in_values[i] = input
+        self.in_values = inputs
 
-        for i in range(self.n_in):
-            self.hid_values[i] = self.__sigmoid(
+        self.hid_values = [
+                self.__sigmoid(
                     sum([
                         self.in_values[j] * self.w_in[j][i]
                         for j
                         in range(self.n_in)]))
+                    for i
+                    in range(self.n_hid)]
 
-        for i in range(self.n_out):
-            self.out_values[i] = self.__sigmoid(
+
+        self.out_values = [
+                self.__sigmoid(
                     sum([
                         self.hid_values[j] * self.w_out[j][i]
                         for j
                         in range(self.n_hid)]))
+                    for i
+                    in range(self.n_out)]
+
 
         return self.out_values[:]
 
@@ -51,18 +54,17 @@ class MLP:
         if len(targets) != self.n_out: raise ValueError('wrong number of targets')
 
         output_deltas = [
-                self.__dsigmoid(out_value) * (target - out_value)
+                self.__dsigmoid(out_value) * out_value * (target - out_value)
                 for target, out_value
                 in zip(targets, self.out_values)]
 
         hidden_deltas = [
-                self.__dsigmoid(
-                    self.hid_values[i] * sum(
-                        [output_deltas[j] * self.w_out[i][j]
-                            for j
-                            in range(self.n_out)]))
-                for i
-                in range(self.n_hid)]
+            self.__dsigmoid(self.hid_values[i]) * self.hid_values[i] * sum(
+                [output_deltas[j] * self.w_out[i][j]
+                 for j
+                 in range(self.n_out)])
+            for i
+            in range(self.n_hid)]
 
         for i in range(self.n_hid):
             for j in range(self.n_out):
@@ -81,4 +83,4 @@ class MLP:
         return 1 / (1 + math.exp(x * -1))
 
     def __dsigmoid(self, x):
-        return 1.0 - x ** 2
+        return 1 - x